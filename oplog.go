d6ded0891d968f9c4d95d2d8c18206a577c2ad9e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-17 08:02:20 +0530

    ping a server for a get request, and print the response

--- oplog.go
+++ oplog.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello")
+	// TODO: change the way this URL is determined
+	url := "http://127.0.0.1:5000/"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	fmt.Printf("%s", body)
 }
