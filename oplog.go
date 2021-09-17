4732caf4667fc3c239c7d1315d91df9cfeae5f55
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-09-17 08:29:30 +0530

    return both response string and status code

--- oplog.go
+++ oplog.go
@@ -15,11 +15,13 @@ type Oprequest struct {
 }
 
 func main() {
-	fmt.Println(makeRequest("popcorn"))
+	res, statusCode := makeRequest("popcorn")
+	fmt.Println(res)
+	fmt.Println(statusCode)
 }
 
-// make request to API and return the response
-func makeRequest(word string) string {
+// make request to API and return the response body and status code
+func makeRequest(word string) (string, int) {
 	// TODO: change the way this URL is determined
 	url := "http://127.0.0.1:5000/reverse"
 
@@ -37,5 +39,8 @@ func makeRequest(word string) string {
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
-	return string(body)
+	if err != nil {
+		log.Fatalln("error reading response", err)
+	}
+	return string(body), resp.StatusCode
 }
